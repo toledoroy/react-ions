c074734ac69c52605ec61f5f3f2da19ac80763a4
Sam Ageloff <sam@getambassador.com>
2016-04-14 15:52:22 -0400

    import flexbox css

--- docs/src/app/layout/Base.js
+++ docs/src/app/layout/Base.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
+import style from '../../www/css/base.scss'
 import Header from './Header'
 import Nav from './Nav'
 
@@ -9,12 +10,16 @@ class Base extends React.Component {
   }
   render() {
     return (
-      <div>
-        <Header page={this.props.params.content} />
-        <Nav />
-        <section role="main">
-          {this.props.children}
-        </section>
+      <div className={style.row}>
+        <nav className={style.nav}>
+          <Nav />
+        </nav>
+        <div className={style.content}>
+          <Header page={this.props.params.content} />
+          <section role='main'>
+            {this.props.children}
+          </section>
+        </div>
       </div>
     )
   }
