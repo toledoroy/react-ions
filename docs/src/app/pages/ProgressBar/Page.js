3e0fb05b78962cf3024b33ec8bc0680f7bfeb748
Ivan Sugerman <ivan@getambassador.com>
2016-04-21 14:20:20 -0400

    print out the props

--- docs/src/app/pages/ProgressBar/Page.js
+++ docs/src/app/pages/ProgressBar/Page.js
@@ -16,6 +16,8 @@ class ProgressBarPage extends React.Component {
             <h3>Progress Bar</h3>
             <p>Toy mouse squeak roll over ears back wide eyed eat the fat cats.</p>
             <ProgressBar value={20} denominator={100} />
+            <h3>Props</h3>
+            <pre>{JSON.stringify(docs[0].props, null, 2) }</pre>
           </div>
         </div>
       </div>
