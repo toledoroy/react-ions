950d53dbd430d61ff73c23325955a33977abacda
Corey Fields <corey@getambassador.com>
2016-04-21 09:39:19 -0400

    modifiy test paths

---
+++ test/ProgressBarLabel.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import ProgressBarLabel from '../src/components/ProgressBar/ProgressBarLabel';
+
+describe('ProgressBarLabel', () => {
+  let wrapper;
+  let showPercentage = true;
+  const text = "Ambassador export";
+  const percentage = 10;
+
+  it('should shallow render itself with proper tags and show percentage', () => {
+    wrapper = shallow(<ProgressBarLabel text={text} showPercentage={showPercentage} percentage={percentage} />);
+
+    expect(wrapper.find('label')).to.have.length(1);
+    expect(wrapper.first().props().className).to.equal('label');
+    expect(wrapper.first().hasClass('label')).to.equal(true);
+    expect(wrapper.childAt(0).text()).to.equal(text);
+    expect(wrapper.first().find('span')).to.have.length(1);
+    expect(wrapper.first().children().length).to.equal(2);
+    expect(wrapper.childAt(1).props().className).to.equal('percentage');
+    expect(wrapper.childAt(1).hasClass('percentage')).to.equal(true);
+  });
+
+  it('should shallow render itself with proper tags and show percentage', () => {
+    showPercentage = false;
+    wrapper = shallow(<ProgressBarLabel text={text} showPercentage={showPercentage} percentage={percentage} />);
+
+    expect(wrapper.find('label')).to.have.length(1);
+    expect(wrapper.first().props().className).to.equal('label');
+    expect(wrapper.first().hasClass('label')).to.equal(true);
+    expect(wrapper.first().children().length).to.equal(1);
+    expect(wrapper.childAt(0).text()).to.equal(text);
+    expect(wrapper.first().find('span')).to.have.length(0);
+  });
+});
