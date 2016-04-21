8879216a7e8db296a95f7eccf4ad1c1df0b5f392
Ivan Sugerman <ivan@getambassador.com>
2016-04-21 18:13:15 -0400

    [#117806965] added some style

--- docs/src/app/modules/PropsList.js
+++ docs/src/app/modules/PropsList.js
@@ -13,27 +13,31 @@ class PropsList extends React.Component {
   render() {
     let propsList = Object.keys(this.props.list).map((name) => {
       return (
-        <tr>
+        <tr key={name}>
           <td>{name}</td>
-          <td>{this.props.list[name].type.name}</td>
-          <td>{this.props.list[name].defaultValue ? this.props.list[name].defaultValue.value : ''}</td>
+          <td className={style['cell-monospaced']}>{this.props.list[name].type.name}</td>
+          <td className={style['cell-monospaced']}>{this.props.list[name].defaultValue ? this.props.list[name].defaultValue.value : ''}</td>
           <td>{this.props.list[name].description}</td>
         </tr>
       )
     });
 
     return (
-      <table>
-        <thead>
-          <th>Name</th>
-          <th>Type</th>
-          <th>Default</th>
-          <th>Description</th>
-        </thead>
-        <tbody>
-          {propsList}
-        </tbody>
-      </table>
+      <div className={style.wrapper}>
+        <table className={style.table}>
+          <thead>
+            <tr>
+              <th className={style['cell-md']}>Name</th>
+              <th className={style['cell-sm']}>Type</th>
+              <th className={style['cell-sm']}>Default</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {propsList}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
