46ef8e5159221c46939e727fab17002f1f93c724
Corey Fields <corey@getambassador.com>
2016-04-22 17:04:36 -0400

    [#117843457] indent

--- docs/src/app/layout/Nav.js
+++ docs/src/app/layout/Nav.js
@@ -11,21 +11,21 @@ class Nav extends React.Component {
       <div role='nav' className={style.nav}>
         <ul>
           <li className={style.active}><NavLink to='/foundations'>Foundations</NavLink>
-          <ul>
-            <li><NavLink to='/foundations/colors'>Colors</NavLink></li>
-            <li><NavLink to='/foundations/typography'>Typography</NavLink></li>
-            <li><NavLink to='/foundations/iconography'>Iconography</NavLink></li>
-            <li><NavLink to='/foundations/layout'>Layout</NavLink></li>
-          </ul>
-        </li>
-        <li><NavLink to='/components'>Components</NavLink>
-          <ul>
-            <li><NavLink to='/components/progress-bar'>Progress Bar</NavLink></li>
-          </ul>
-        </li>
-        <li><NavLink to='/patterns'>Patterns</NavLink></li>
-        <li><NavLink to='/resources'>Resources</NavLink></li>
-      </ul>
+            <ul>
+              <li><NavLink to='/foundations/colors'>Colors</NavLink></li>
+              <li><NavLink to='/foundations/typography'>Typography</NavLink></li>
+              <li><NavLink to='/foundations/iconography'>Iconography</NavLink></li>
+              <li><NavLink to='/foundations/layout'>Layout</NavLink></li>
+            </ul>
+          </li>
+          <li className={style.active}><NavLink to='/components'>Components</NavLink>
+            <ul>
+              <li><NavLink to='/components/progress-bar'>Progress Bar</NavLink></li>
+            </ul>
+          </li>
+          <li><NavLink to='/patterns'>Patterns</NavLink></li>
+          <li><NavLink to='/resources'>Resources</NavLink></li>
+        </ul>
     </div>
     )
   }
