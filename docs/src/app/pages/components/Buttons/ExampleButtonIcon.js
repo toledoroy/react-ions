346ddf352d003bdd32e43f0f0bebf77869e90fbc
Sam Ageloff <sam@getambassador.com>
2016-04-26 18:00:46 -0400

    svg inlining for firefox

--- docs/src/app/pages/components/Buttons/ExampleButtonIcon.js
+++ docs/src/app/pages/components/Buttons/ExampleButtonIcon.js
@@ -6,6 +6,7 @@ const ExampleButtonIcon = () => (
   <Button>
     <Icon name='icon-upload-2-1' height='14' width='14' fill='#fff'></Icon>
     <span>Button with Icon</span>
+    <Icon name='icon-upload-2-1' height='14' width='14' fill='#fff'></Icon>
   </Button>
 )
 
