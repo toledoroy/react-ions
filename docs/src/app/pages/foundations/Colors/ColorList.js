7bcb8596749f12c782a554e5bf47c34e0a27399f
Marko <marko.corokalo@gmail.com>
2016-04-26 14:20:11 +0200

    [#118032637] Add and style colors page

---
+++ docs/src/app/pages/foundations/Colors/ColorList.js
@@ -0,0 +1,55 @@
+const colorList = {
+  neutralColors: [
+    {
+      sass: '$variable',
+      hex: '#ECF0F0'
+    },{
+      sass: '$variable',
+      hex: '#F7F7F9'
+    },{
+      sass: '$variable',
+      hex: '#DEE0E3'
+    },{
+      sass: '$variable',
+      hex: '#9198A0'
+    }
+  ],
+  primaryColors: [
+    {
+      sass: '$variable',
+      hex: '#3C97D3'
+    },{
+      sass: '$variable',
+      hex: '#3178B7'
+    },{
+      sass: '$variable',
+      hex: '#1B252F'
+    },{
+      sass: '$variable',
+      hex: '#243140'
+    },{
+      sass: '$variable',
+      hex: '#2D3E50'
+    },{
+      sass: '$variable',
+      hex: '#4F6A92'
+    }
+  ],
+  secondaryColors: [
+    {
+      sass: '$variable',
+      hex: '#26B894'
+    },{
+      sass: '$variable',
+      hex: '#F29C1E'
+    },{
+      sass: '$variable',
+      hex: '#E54C3B'
+    },{
+      sass: '$variable',
+      hex: '#62CAF3'
+    }
+  ]
+};
+
+export default colorList;
--- docs/src/app/pages/foundations/Colors/Page.js
+++ docs/src/app/pages/foundations/Colors/Page.js
@@ -1,23 +1,64 @@
 import React from 'react'
 import Breadcrumb from 'react-conventions/lib/Breadcrumb'
-import styles from '../../../../www/css/content'
+import Color from '../../components/Colors/Color'
+import baseStyles from '../../../../www/css/content'
+import list from './ColorList'
+import localStyles from './styles.scss';
 
 class ColorsPage extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+
+    let neutralColorList = list.neutralColors.map((color, index) =>
+      <div className={localStyles['color-wrapper']}>
+        <Color hex={color.hex} sass={color.sass} />
+      </div>
+    );
+    let primaryColorList = list.primaryColors.map((color, index) =>
+      <div className={localStyles['color-wrapper']}>
+        <Color hex={color.hex} sass={color.sass} />
+      </div>
+    );
+    let secondaryColorList = list.secondaryColors.map((color, index) =>
+      <div className={localStyles['color-wrapper']}>
+        <Color hex={color.hex} sass={color.sass} />
+      </div>
+    );
+
     return (
       <div>
-        <div className={styles.content}>
-          <div className={styles.block}>
+        <div className={baseStyles.content}>
+          <div className={baseStyles.block}>
             <h3>Color</h3>
             <p>Ambassador uses a specific color palette to communicate meaning, convey visual differentation, and provide a consistent look and feel.</p>
           </div>
-          <div className={styles.block}>
+          <div className={baseStyles.block}>
             <h3>Neutrals</h3>
             <p>Ambassador uses cool-toned grays injected with a hint of teal to create a lively and modern mood for the interface.</p>
             <p>In general, color is used sparingly to keep the content center stage and not distract end users.</p>
+
+            <div className={localStyles['color-list']}>
+              {neutralColorList}
+            </div>
+          </div>
+          <div className={baseStyles.block}>
+            <h3>Primary Colors</h3>
+            <p>Ambassador’s primary brand color is a bright, friendly blue. We also use some darker blues for various other use cases.</p>
+
+            <div className={localStyles['color-list']}>
+              {primaryColorList}
+            </div>
+          </div>
+          <div className={baseStyles.block}>
+            <h3>Secondary Colors</h3>
+            <p>We use a broad range of secondary colors that complement the neutral and primary colors.</p>
+            <p>The level of saturation is vibrant but not neon, mellow but not muted.</p>
+
+            <div className={localStyles['color-list']}>
+              {secondaryColorList}
+            </div>
           </div>
         </div>
       </div>
