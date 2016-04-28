87c4b21d0b00c464a56d7baa5362c1e0a17d60b7
Sam Ageloff <sam@getambassador.com>
2016-04-28 09:30:41 -0400

    a couple of hex color var reassignments

--- docs/src/app/pages/foundations/Colors/ColorList.js
+++ docs/src/app/pages/foundations/Colors/ColorList.js
@@ -3,13 +3,13 @@ const colorList = {
     {
       sass: '$neutral-1',
       hex: '#ECF0F0'
-    },{
+    }, {
       sass: '$neutral-2',
       hex: '#F7F7F9'
-    },{
+    }, {
       sass: '$neutral-3',
       hex: '#DEE0E3'
-    },{
+    }, {
       sass: '$neutral-4',
       hex: '#9198A0'
     }
@@ -18,35 +18,35 @@ const colorList = {
     {
       sass: '$primary-1',
       hex: '#3C97D3'
-    },{
+    }, {
       sass: '$primary-2',
       hex: '#3178B7'
-    },{
+    }, {
       sass: '$primary-3',
       hex: '#1B252F'
-    },{
+    }, {
       sass: '$primary-4',
       hex: '#243140'
-    },{
+    }, {
       sass: '$primary-5',
       hex: '#2D3E50'
-    },{
+    }, {
       sass: '$primary-6',
       hex: '#4F6A92'
     }
   ],
   secondaryColors: [
     {
-      sass: '$green',
+      sass: '$success',
       hex: '#26B894'
-    },{
-      sass: '$orange',
+    }, {
+      sass: '$danger',
       hex: '#F29C1E'
-    },{
-      sass: '$red',
+    }, {
+      sass: '$warning',
       hex: '#E54C3B'
-    },{
-      sass: '$aqua',
+    }, {
+      sass: '$info',
       hex: '#62CAF3'
     }
   ]
