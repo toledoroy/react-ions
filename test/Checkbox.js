4f48bd6c004a35cdd95101ef67f335dd1b864398
Corey Fields <corey@getambassador.com>
2016-04-29 09:59:41 -0400

    [#116804327] first test

---
+++ test/Checkbox.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Checkbox from '../src/components/Checkbox/Checkbox';
+
+describe('Checkbox', () => {
+  let wrapper;
+
+  it('should shallow render itself', () => {
+    wrapper = shallow(<Checkbox value="test" label="Test label"></Radio>);
+
+    expect(wrapper.find('div')).to.have.length(3);
+    expect(wrapper.find('input')).to.have.length(1);
+    expect(wrapper.find('label')).to.have.length(1);
+
+    expect(wrapper.hasClass('checkbox-component')).to.equal(true);
+    expect(wrapper.childAt(1).childAt(0).hasClass('checkbox-input')).to.equal(true);
+    expect(wrapper.childAt(1).childAt(1).hasClass('label-right')).to.equal(true);
+  });
+
+  // it('should be disabled', () => {
+  //   wrapper = shallow(<Radio value="test" label="Test label" disabled></Radio>);
+  //   expect(wrapper.hasClass('radio-component')).to.equal(true);
+  //   expect(wrapper.hasClass('radio-disabled')).to.equal(true);
+
+  //   wrapper = mount(<Radio value="test" label="Test label" disabled></Radio>);
+  //   expect(wrapper.find('input').node.hasAttribute('disabled')).to.equal(true);
+  // });
+
+  // it('should have an extra class', () => {
+  //   wrapper = shallow(<Radio value="test" label="Test label" optClass="radio-error"></Radio>);
+
+  //   expect(wrapper.hasClass('radio-component')).to.equal(true);
+  //   expect(wrapper.hasClass('radio-error')).to.equal(true);
+  // });
+
+  // it('should have the label on the left side', () => {
+  //   wrapper = shallow(<Radio value="test" label="Test label" labelPosition="left"></Radio>);
+
+  //   expect(wrapper.hasClass('radio-component'));
+  //   expect(wrapper.childAt(1).childAt(0).hasClass('label-left')).to.equal(true);
+  //   expect(wrapper.childAt(1).childAt(1).hasClass('radio-input')).to.equal(true);
+  // });
+});
