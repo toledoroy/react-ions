1956dad06d0a306c0518d0bac0cc29576e0e2e5d
Sam Ageloff <sam@getambassador.com>
2016-05-03 17:02:06 -0400

    working on home page view

--- docs/src/app/pages/components/Buttons/ExampleButtonAsLink.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Button from 'react-conventions/lib/Button'
-
-const ExampleButtonAsLink = () => (
-  <Button url='http://google.com'>I am a Link that looks like a button</Button>
-)
-
-export default ExampleButtonAsLink
