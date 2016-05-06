d9f91f3c46663f152d317edc6b0d040d3fea1dde
Ivan Sugerman <ivan@getambassador.com>
2016-05-06 14:42:23 -0400

    export generator adjustments

--- scripts/genExport.js
+++ scripts/genExport.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 fs.readdir('./src/components', function(err, data) {
 
   var components = data.filter(function(f){ return (/^[A-Z]/.test(f)); });
-  var requiresList = components.map(function(f){ return 'var ' + f + ' = require(./' + f + ')'; });
+  var requiresList = components.map(function(f){ return 'var ' + f + ' = require(\'./' + f + '\');'; });
   var exportsList = components.map(function(f){ return 'exports.' + f + ' = ' + f + '.default;'; });
 
   var output  = '\'use strict\';';
