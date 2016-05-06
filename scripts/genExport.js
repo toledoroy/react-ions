3a8fa1c71cc18181ebdcacc909d2c73591954711
Ivan Sugerman <ivan@getambassador.com>
2016-05-06 12:14:40 -0400

    generate root export

---
+++ scripts/genExport.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var fs = require('fs');
+
+fs.readdir('./src/components', function(err, data) {
+
+  var components = data.filter(function(f){ return (/^[A-Z]/.test(f)); });
+  var requiresList = components.map(function(f){ return 'var ' + f + ' = require(./' + f + ')'; });
+  var exportsList = components.map(function(f){ return 'exports.' + f + ' = ' + f + '.default;'; });
+
+  var output  = '\'use strict\';';
+      output += '\n';
+      output += requiresList.join('\n');
+      output += '\n';
+      output += exportsList.join('\n');
+
+  fs.writeFile('./lib/index.js', output);
+});
