347fdb3cd02ae184aa84612ba2a033cc674641ff
Corey Fields <corey@getambassador.com>
2016-05-09 13:30:33 -0400

    up coverage threshold on lines

--- test/config/karma.local.conf.js
+++ test/config/karma.local.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
       statements: 95,
       branches: 95,
       functions: 95,
-      lines: 90
+      lines: 95
     },
 
     webpack: { //kind of a copy of your webpack config
