871f2f22941239a478bc3178ed37e0ae30bf2d31
Ivan Sugerman <ivan@getambassador.com>
2016-05-10 07:46:00 -0400

    update component export

--- scripts/gen-export.js
+++ scripts/gen-export.js
@@ -2,24 +2,18 @@
 
 var fs = require('fs');
 
+function _getComponentExport(component) {
+  fs.readFile('./lib/components/' + component + '/index.js', function(error, data) {
+    var componentData = data.toString().replace('./', './components/');
+    fs.writeFile('./lib/' + component + '.js', componentData);
+  });
+}
+
 fs.readdir('./src/components', function(err, data) {
-
   var components = data.filter(function(f){ return (/^[A-Z]/.test(f)); });
-  var requiresList = components.map(function(f){ return 'var ' + f + ' = require(\'./components/' + f + '\');'; });
-  var exportsList = components.map(function(f){ return 'exports.' + f + ' = ' + f + '.default;'; });
-
-  // Generate master export
-  var output  = '\'use strict\';';
-      output += '\n';
-      output += requiresList.join('\n');
-      output += '\n';
-      output += exportsList.join('\n');
-
-  fs.writeFile('./lib/index.js', output);
 
   // Generate individual component exports
-  for (var i = 0; i < requiresList.length; i++) {
-    var componentOutput = requiresList[i] + exportsList[i];
-    fs.writeFile('./lib/' + components[i] + '.js', componentOutput);
+  for (var i = 0; i < components.length; i++) {
+    _getComponentExport(components[i]);
   }
 });
