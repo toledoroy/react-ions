c3b76967320a6568cfe640933bd04ae50fcd2875
Marko <marko.corokalo@gmail.com>
2016-05-11 17:23:58 +0200

    [#118922211] Update tests

--- test/Breadcrumb.js
+++ test/Breadcrumb.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Breadcrumb from '../src/components/Breadcrumb/Breadcrumb';
 import Icon from '../src/components/Icon/Icon';
 
@@ -14,8 +14,8 @@ describe('Breadcrumb', () => {
 
   it('should not render any children', () => {
     wrapper = shallow(<Breadcrumb routes={routes} />);
-    expect(wrapper.find('div')).to.have.length(1);
-    expect(wrapper.first().hasClass('breadcrumb')).to.equal(true);
+    expect(wrapper.find('div')).to.have.length(2);
+    expect(wrapper.childAt(0).hasClass('breadcrumb')).to.equal(true);
     expect(wrapper.find('h2')).to.have.length(0);
     expect(wrapper.find('span')).to.have.length(0);
     expect(wrapper.find(Icon)).to.have.length(0);
@@ -28,11 +28,11 @@ describe('Breadcrumb', () => {
     });
 
     wrapper = shallow(<Breadcrumb routes={routes} />);
-    expect(wrapper.find('div')).to.have.length(1);
-    expect(wrapper.first().hasClass('breadcrumb')).to.equal(true);
+    expect(wrapper.find('div')).to.have.length(2);
+    expect(wrapper.childAt(0).hasClass('breadcrumb')).to.equal(true);
 
     expect(wrapper.find('h2')).to.have.length(1);
-    expect(wrapper.childAt(0).hasClass('primary')).to.equal(true);
+    expect(wrapper.childAt(0).childAt(0).hasClass('primary')).to.equal(true);
     expect(wrapper.contains(<h2 className='primary'>Foundations</h2>)).to.equal(true);
 
     expect(wrapper.find('span')).to.have.length(0);
@@ -47,23 +47,23 @@ describe('Breadcrumb', () => {
     });
 
     wrapper = shallow(<Breadcrumb routes={routes} />);
-    expect(wrapper.find('div')).to.have.length(1);
-    expect(wrapper.first().hasClass('breadcrumb')).to.equal(true);
+    expect(wrapper.find('div')).to.have.length(2);
+    expect(wrapper.childAt(0).hasClass('breadcrumb')).to.equal(true);
 
     expect(wrapper.find('h2')).to.have.length(1);
-    expect(wrapper.childAt(0).hasClass('primary')).to.equal(true);
+    expect(wrapper.childAt(0).childAt(0).hasClass('primary')).to.equal(true);
     expect(wrapper.contains(<h2 className='primary'>Foundations</h2>)).to.equal(true);
 
     expect(wrapper.find('span')).to.have.length(1);
-    expect(wrapper.childAt(2).props().className).to.equal('secondary');
+    expect(wrapper.childAt(0).childAt(2).props().className).to.equal('secondary');
     expect(wrapper.contains(<span className='secondary'>Colors</span>)).to.equal(true);
 
     expect(wrapper.find(Icon)).to.have.length(1);
-    expect(wrapper.childAt(1).props().name).to.equal('icon-arrow-68');
-    expect(wrapper.childAt(1).props().className).to.equal('icon-arrow-68');
-    expect(wrapper.childAt(1).props().width).to.equal('14');
-    expect(wrapper.childAt(1).props().height).to.equal('14');
-    expect(wrapper.childAt(1).props().color).to.equal('#879098');
+    expect(wrapper.childAt(0).childAt(1).props().name).to.equal('icon-arrow-68');
+    expect(wrapper.childAt(0).childAt(1).props().className).to.equal('icon-arrow-68');
+    expect(wrapper.childAt(0).childAt(1).props().width).to.equal('14');
+    expect(wrapper.childAt(0).childAt(1).props().height).to.equal('14');
+    expect(wrapper.childAt(0).childAt(1).props().color).to.equal('#879098');
   });
 
   it('should shallow render itself with a primary tag, two secondary tags, and two arrow icons', () => {
@@ -73,34 +73,34 @@ describe('Breadcrumb', () => {
     });
 
     wrapper = shallow(<Breadcrumb routes={routes} />);
-    expect(wrapper.find('div')).to.have.length(1);
-    expect(wrapper.first().hasClass('breadcrumb')).to.equal(true);
+    expect(wrapper.find('div')).to.have.length(2);
+    expect(wrapper.childAt(0).hasClass('breadcrumb')).to.equal(true);
 
     expect(wrapper.find('h2')).to.have.length(1);
-    expect(wrapper.childAt(0).hasClass('primary')).to.equal(true);
+    expect(wrapper.childAt(0).childAt(0).hasClass('primary')).to.equal(true);
     expect(wrapper.contains(<h2 className='primary'>Foundations</h2>)).to.equal(true);
 
     expect(wrapper.find('span')).to.have.length(2);
-    expect(wrapper.childAt(2).props().className).to.equal('secondary');
+    expect(wrapper.childAt(0).childAt(2).props().className).to.equal('secondary');
     expect(wrapper.contains(<span className='secondary'>Colors</span>)).to.equal(true);
 
     expect(wrapper.find(Icon)).to.have.length(2);
-    expect(wrapper.childAt(1).props().name).to.equal('icon-arrow-68');
-    expect(wrapper.childAt(1).props().className).to.equal('icon-arrow-68');
-    expect(wrapper.childAt(1).props().width).to.equal('14');
-    expect(wrapper.childAt(1).props().height).to.equal('14');
-    expect(wrapper.childAt(1).props().color).to.equal('#879098');
+    expect(wrapper.childAt(0).childAt(1).props().name).to.equal('icon-arrow-68');
+    expect(wrapper.childAt(0).childAt(1).props().className).to.equal('icon-arrow-68');
+    expect(wrapper.childAt(0).childAt(1).props().width).to.equal('14');
+    expect(wrapper.childAt(0).childAt(1).props().height).to.equal('14');
+    expect(wrapper.childAt(0).childAt(1).props().color).to.equal('#879098');
 
     expect(wrapper.find('span')).to.have.length(2);
-    expect(wrapper.childAt(4).props().className).to.equal('secondary');
+    expect(wrapper.childAt(0).childAt(4).props().className).to.equal('secondary');
     expect(wrapper.contains(<span className='secondary'>Test</span>)).to.equal(true);
 
     expect(wrapper.find(Icon)).to.have.length(2);
-    expect(wrapper.childAt(3).props().name).to.equal('icon-arrow-68');
-    expect(wrapper.childAt(3).props().className).to.equal('icon-arrow-68');
-    expect(wrapper.childAt(3).props().width).to.equal('14');
-    expect(wrapper.childAt(3).props().height).to.equal('14');
-    expect(wrapper.childAt(3).props().color).to.equal('#879098');
+    expect(wrapper.childAt(0).childAt(3).props().name).to.equal('icon-arrow-68');
+    expect(wrapper.childAt(0).childAt(3).props().className).to.equal('icon-arrow-68');
+    expect(wrapper.childAt(0).childAt(3).props().width).to.equal('14');
+    expect(wrapper.childAt(0).childAt(3).props().height).to.equal('14');
+    expect(wrapper.childAt(0).childAt(3).props().color).to.equal('#879098');
   });
 
   it('should shallow render and display two levels even if a root was not passed in', () => {
@@ -116,15 +116,15 @@ describe('Breadcrumb', () => {
     ];
 
     wrapper = shallow(<Breadcrumb routes={routes} />);
-    expect(wrapper.find('div')).to.have.length(1);
-    expect(wrapper.first().hasClass('breadcrumb')).to.equal(true);
+    expect(wrapper.find('div')).to.have.length(2);
+    expect(wrapper.childAt(0).hasClass('breadcrumb')).to.equal(true);
 
     expect(wrapper.find('h2')).to.have.length(1);
-    expect(wrapper.childAt(0).hasClass('primary')).to.equal(true);
+    expect(wrapper.childAt(0).childAt(0).hasClass('primary')).to.equal(true);
     expect(wrapper.contains(<h2 className='primary'>Foundations</h2>)).to.equal(true);
 
     expect(wrapper.find('span')).to.have.length(1);
-    expect(wrapper.childAt(2).props().className).to.equal('secondary');
+    expect(wrapper.childAt(0).childAt(2).props().className).to.equal('secondary');
     expect(wrapper.contains(<span className='secondary'>Test</span>)).to.equal(true);
   });
 });
