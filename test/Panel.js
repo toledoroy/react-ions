54df9257853ec82bdccf564222a3138bfdff468f
Sam Ageloff <sam@getambassador.com>
2016-05-13 09:51:15 -0400

    [#116805635] trying to test this thing

--- test/Panel.js
+++ test/Panel.js
@@ -1,36 +1,25 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-import {PanelGroup, Panel, PanelHeader, PanelContent} from '../src/components/PanelGroup'
+import { shallow } from 'enzyme';
+import {Panel, PanelHeader, PanelContent} from '../src/components/PanelGroup'
 
 describe('Panel', () => {
   it('should shallow render itself', () => {
-    const wrapper = shallow(
-      <PanelGroup>
-        <Panel>
-          <PanelHeader></PanelHeader>
-          <PanelContent></PanelContent>
-        </Panel>
-      </PanelGroup>);
-    const panel = wrapper.childAt(0);
-    console.log(panel.html());
-    // expect(panel.find('div')).to.have.length(1);
-    // expect(panel.hasClass('panel')).to.equal(true);
+    const wrapper = shallow(<Panel title='Rating' contextIcon='icon-star-1'><PanelHeader optClass='rating-specific'></PanelHeader><PanelContent></PanelContent></Panel>);
+    expect(wrapper.props().title).to.equal('Rating');
+
+    // expect(panelHeader.hasClass('panel-header')).to.equal(true);
+    // expect(panelContent.hasClass('panel-content')).to.equal(true);
   });
 });
 
 
 /* wrapper
-<div class="panel-group">
-  <div class="panel">
-    <div class="panel-header">
-      <div>
-        <div class="title-group"></div>
-        <div class="toggle-icon">
-          <svg name="icon-delete-1" height="12" width="12"><use xlink:href="/_karma_webpack_//13909255dfd9387151d92875db15505a.svg#icon-delete-1"></use></svg>
-        </div>
-      </div>
-    </div>
-    <div class="panel-content"></div>
+<div class="panel">
+  <div class="panel-header"><div>
+  <div class="title-group"></div>
+  <div class="toggle-icon">
+    <svg name="icon-delete-1" height="12" width="12"><use xlink:href="/_karma_webpack_//13909255dfd9387151d92875db15505a.svg#icon-delete-1"></use></svg>
   </div>
 </div>
+</div><div class="panel-content"></div></div>
 */
--- test/PanelHeader.js
+++ test/PanelHeader.js
@@ -4,12 +4,12 @@ import {PanelGroup, Panel, PanelHeader, PanelContent} from '../src/components/Pa
 
 describe('Panel Header', () => {
   it('should shallow render itself', () => {
-    const wrapper = shallow(<PanelGroup><Panel><PanelHeader title='Rating' contextIcon='icon-star-1' />a<PanelContent optClass='rating-specific'><p>{content.lorum1}</p></PanelContent></Panel></PanelGroup>);
+    const wrapper = shallow(<PanelGroup><Panel><PanelHeader title='Rating' contextIcon='icon-star-1' /><PanelContent optClass='rating-specific'><p>Content here</p></PanelContent></Panel></PanelGroup>);
     const panelHeader = wrapper.childAt(0).childAt(0);
     // https://github.com/airbnb/enzyme/issues/306
-    console.log(panelHeader.html());
-    expect(panelHeader.find('div')).to.have.length(1);
-    expect(panelHeader.hasClass('panel-header')).to.equal(true);
+    // console.log(panelHeader.html());
+    // expect(panelHeader.find('div')).to.have.length(1);
+    // expect(panelHeader.hasClass('panel-header')).to.equal(true);
   });
 
 });
@@ -32,5 +32,15 @@ describe('Panel Header', () => {
 */
 
 /*
-<div class="panel"><div class="panel-header"><div><div class="title-group"></div><div class="toggle-icon"><svg name="icon-delete-1" height="12" width="12"><use xlink:href="/_karma_webpack_//13909255dfd9387151d92875db15505a.svg#icon-delete-1"></use></svg></div></div></div><div class="panel-content"></div></div>
- */
+<div class="panel-header">
+  <div>
+    <div class="title-group">
+      <svg name="icon-star-1" height="14" width="14"><use xlink:href="/_karma_webpack_//13909255dfd9387151d92875db15505a.svg#icon-star-1"></use></svg>
+      <h4>Rating</h4>
+    </div>
+    <div class="toggle-icon">
+      <svg name="icon-delete-1" height="12" width="12"><use xlink:href="/_karma_webpack_//13909255dfd9387151d92875db15505a.svg#icon-delete-1"></use></svg>
+    </div>
+  </div>
+</div>
+*/
