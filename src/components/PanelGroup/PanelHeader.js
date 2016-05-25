fa010e65ed7af7a3ee1b2f432bf7abf8b7a19e1c
Sam Ageloff <sam@getambassador.com>
2016-05-25 17:07:31 -0400

    [#120243175] fixing icon size, adding an icon, etc.

--- src/components/PanelGroup/PanelHeader.js
+++ src/components/PanelGroup/PanelHeader.js
@@ -17,6 +17,10 @@ class PanelHeader extends React.Component {
      */
     contextIcon: React.PropTypes.string,
     /**
+     * The size of the context icon
+     */
+    contextIconSize: React.PropTypes.string,
+    /**
      * A node that can be passed to the left of the header
      */
     contextNode: React.PropTypes.node,
@@ -41,7 +45,7 @@ class PanelHeader extends React.Component {
           <div>
             <div className={style['title-group']}>
               {this.props.contextNode ? <div className={style['context-node']}>{this.props.contextNode}</div> : null}
-              {this.props.contextIcon ? <div className={style['context-icon']}><Icon name={this.props.contextIcon} height='14' width='14' /></div> : null}
+              {this.props.contextIcon ? <div className={style['context-icon']}><Icon name={this.props.contextIcon} height={this.props.contextIconSize} width={this.props.contextIconSize} /></div> : null}
               {this.props.title ? <h4>{this.props.title}</h4> : null}
             </div>
             <div className={style['toggle-icon']}>
