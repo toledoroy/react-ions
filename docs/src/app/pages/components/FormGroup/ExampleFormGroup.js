087176dbcc15a1b0ffc8ab4f5e5308747110f7e5
Sam Ageloff <sam@getambassador.com>
2016-05-25 09:57:35 -0400

    [#120220077] setting componentWillReceiveProps

--- docs/src/app/pages/components/FormGroup/ExampleFormGroup.js
+++ docs/src/app/pages/components/FormGroup/ExampleFormGroup.js
@@ -38,29 +38,29 @@ class ExampleFormGroup extends React.Component {
 
   state = {
     schema: {
-      'subject':{
-        'value':'This is my subject'
+      'subject': {
+        'value': 'This is my subject'
       },
       'message': {
-        'value':'This is my message'
+        'value': 'This is my message'
       },
       'radio': {
-        'value':''
+        'value':'option_2'
       },
       'select': {
-        'value': ''
+        'value': '1'
       },
       'email': {
-        'value':true
+        'value': true
       },
       'facebook': {
-        'value':false
+        'value': false
       },
       'twitter': {
-        'value':true
+        'value': true
       },
       'linkedin': {
-        'value':false
+        'value': false
       }
     }
   }
@@ -72,6 +72,34 @@ class ExampleFormGroup extends React.Component {
   handleSubmit = (event, fields) => {
     event.preventDefault();
     alert(JSON.stringify(fields, 2, null));
+  }
+
+  resetForm = () => {
+    this.setState({
+      schema: {
+        subject: {
+          value: ''
+        },
+        message: {
+          value: ''
+        },
+        radio: {
+          value: 'option_1'
+        },
+        select: {
+          value: '0'
+        },
+        email: {
+          value: false
+        },
+        twitter: {
+          value: false
+        },
+        facebook: {
+          value: true
+        }
+      }
+    });
   }
 
   render() {
@@ -110,6 +138,7 @@ class ExampleFormGroup extends React.Component {
         </fieldset>
 
         <Button type="submit">Submit</Button>
+        <Button optClass='inverted' onClick={this.resetForm}>Reset</Button>
       </FormGroup>
     );
   }
