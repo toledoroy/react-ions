30316fcae13884613e54aac7ff1967ab63460a14
Sam Ageloff <sam@getambassador.com>
2016-06-01 13:31:10 -0400

    fixed awkward grammar

--- docs/src/app/pages/components/Dropdown/ExampleDropdownButton.js
+++ docs/src/app/pages/components/Dropdown/ExampleDropdownButton.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Dropdown from 'react-conventions/lib/Dropdown'
 import Button from 'react-conventions/lib/Button'
+import Badge from 'react-conventions/lib/Badge'
+import styles from './styles'
 
 const ExampleDropdownButton = () => (
   <Dropdown trigger={<Button>Dropdown</Button>}>
-    This is the contents.
+    <div className={styles.wrapper}>
+      <Badge icon='icon-check-1-1' theme='success' /><span>Dropdown content here.</span>
+    </div>
   </Dropdown>
 )
 
--- docs/src/app/pages/components/Dropdown/ExampleDropdownCallback.js
+++ docs/src/app/pages/components/Dropdown/ExampleDropdownCallback.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Dropdown from 'react-conventions/lib/Dropdown'
 import Button from 'react-conventions/lib/Button'
-import styles from './styles.scss'
+import Badge from 'react-conventions/lib/Badge'
+import styles from './styles'
 
 class ExampleDropdownCallback extends React.Component {
   constructor(props) {
@@ -20,7 +21,9 @@ class ExampleDropdownCallback extends React.Component {
     return (
       <div>
         <Dropdown trigger={<u>dropdown here</u>} isOpened={this.state.isOpened}>
-          This is the contents.
+          <div className={styles.wrapper}>
+            <Badge icon='icon-check-1-1' theme='success' /><span>Dropdown content here.</span>
+          </div>
         </Dropdown>
         <p className={styles['float-right']}>And here you can <a href="#" onClick={this.handleOpen}>open it remotely</a>.</p>
       </div>
