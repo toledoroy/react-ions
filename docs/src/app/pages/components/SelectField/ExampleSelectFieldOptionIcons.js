0ca5e6696afdf1c89379370cea0bc9e8111a43e2
Ivan Sugerman <ivan@getambassador.com>
2016-06-10 14:34:06 -0400

    added ability to provide an icon for each option

---
+++ docs/src/app/pages/components/SelectField/ExampleSelectFieldOptionIcons.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SelectField from 'react-conventions/lib/SelectField/SelectField'
+
+const options = [
+  { value: '0', display: 'test 1', someOtherProp: true, icon: 'icon-megaphone-1' },
+  { value: '1', display: 'test 2', someOtherProp: false, icon: 'icon-slack-1', iconColor: '#3C97D3' }
+];
+
+const ExampleSelectFieldOptionIcons = () => (
+  <SelectField
+    options={options}
+    valueProp='value'
+    displayProp='display' />
+)
+
+export default ExampleSelectFieldOptionIcons
