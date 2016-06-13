1f76ed5efcfab994130c7ee78a13a8a053068ed8
Sam Ageloff <sam@getambassador.com>
2016-06-13 14:34:46 -0400

    enabled grid

--- docs/src/app/pages/foundations/Layout/Page.js
+++ docs/src/app/pages/foundations/Layout/Page.js
@@ -1,13 +1,49 @@
 import React from 'react'
-import styles from 'private/css/content'
+import classNames from 'classnames/bind'
+import style from 'private/css/content'
+import grid from 'react-conventions/styles/common/grid'
 
 const LayoutPage = (props) => {
+  const cx = classNames.bind(style);
+  var boxColumnClass = cx(style['box'], style['col']);
+
   return (
-    <div>
-      <div className={styles.content}>
-        <div className={styles.block}>
-          <h3>Layout</h3>
-          <p>Toy mouse squeak roll over ears back wide eyed eat the fat cats.</p>
+    <div className={style.content}>
+      <div className={style.block}>
+        <h3>Layout</h3>
+        <p>We use a modified version of <a href='http://flexboxgrid.com' target='_blank'>Flexbox Grid</a>.</p>
+        <div className='row'>
+          <code className='col-xs-12'>col-xs-12 col-sm-6 col-lg-3</code>
+          <div className='col-xs-12 col-sm-6 col-lg-3'>
+            <div className={style.box}></div>
+          </div>
+          <div className='col-xs-12 col-sm-6 col-lg-3'>
+            <div className={style.box}></div>
+          </div>
+          <div className='col-xs-12 col-sm-6 col-lg-3'>
+            <div className={style.box}></div>
+          </div>
+          <div className='col-xs-12 col-sm-6 col-lg-3'>
+            <div className={style.box}></div>
+          </div>
+        </div>
+        <div className='row'>
+          <code className='col-xs-12'>col-xs-12 col-lg-8</code>
+          <div className='col-xs-12 col-lg-8'>
+            <div className={style.box}></div>
+          </div>
+          <div className='col-xs-12 col-lg-4'>
+            <div className={boxColumnClass}>
+              <div className='row'>
+                <div className='col-xs-12 col-lg-6'>
+                  <div className={style.box}></div>
+                </div>
+                <div className='col-xs-12 col-lg-6'>
+                  <div className={style.box}></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
