1468f773f80c8f4b9916d7d68c5aa74a91b443d4
Sam Ageloff <sam@getambassador.com>
2016-06-17 10:57:01 -0400

    added split on spaces

--- src/components/internal/OptClass.js
+++ src/components/internal/OptClass.js
@@ -3,8 +3,8 @@ import classNames from 'classnames/bind'
 const OptClass = (context, internal, optClass) => {
   const cx = classNames.bind(context)
 
-  optClass = typeof optClass === 'string' ? [optClass] : optClass
-  internal = typeof internal === 'string' ? [internal] : internal
+  optClass = typeof optClass === 'string' ? optClass.split(' ') : optClass
+  internal = typeof internal === 'string' ? internal.split(' ') : internal
   let arr = [internal]
 
   if (optClass !== undefined) {
