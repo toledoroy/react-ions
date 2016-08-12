5dd1b2f2bd1bd39e1e29a3bf08ed448e327e65df
jakedunahee <jake@getambassador.com>
2016-08-12 10:53:01 -0400

    [#128189873] Updated spinner component to be stateless

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,76 +2,67 @@ import React from 'react'
 import style from './style.scss'
 import classNames from 'classnames/bind'
 
-class Spinner extends React.Component {
-  constructor(props) {
-    super(props)
+const Spinner = (props) => {
+  const cx = classNames.bind(style)
+  const loadingClass = props.loading ? style['loading'] : null
+  const spinnerPosition = props.position ? style[props.position] : style['absolute']
+  const spinnerHidden = props.loading ? '' : style['hidden']
+  const spinnerWrap = cx(style['spinner-wrap'], loadingClass, spinnerPosition, props.optClass, spinnerHidden)
+  const spinnerClass = cx(style[props.type])
+
+  const getStyle = () => {
+    return props.color ? { backgroundColor: props.color } : null
   }
 
-  static defaultProps = {
-    loading: false
-  }
-
-  state = {
-    loading: this.props.loading
-  }
-
-  static propTypes = {
-    /**
-     * Whether the spinner is displayed
-     */
-    loading: React.PropTypes.bool,
-    /**
-     * CSS positioning options for the loader. By default, the spinner will be positioned
-     * in the center of any element with relative positioning.
-     */
-    position: React.PropTypes.oneOf(['fixed', 'inline']),
-    /**
-     * The type of loader you want to display.
-     */
-    type: React.PropTypes.oneOf(['spinner-dots', 'spinner-bounce']).isRequired,
-    /**
-     * The hex code of the loader color.
-     */
-    color: React.PropTypes.string,
-    /**
-     * Optional styles to add to the spinner.
-     */
-    optClass: React.PropTypes.string
-  }
-
-  getStyle = () => {
-    return this.props.color ? { backgroundColor: this.props.color } : null
-  }
-
-  innerHtml = () => {
-    if (this.props.type === 'spinner-dots') {
+  const innerHtml = () => {
+    if (props.type === 'spinner-dots') {
       return <span>
-               <span className={style.dot1} style={this.getStyle()}></span>
-               <span className={style.dot2} style={this.getStyle()}></span>
+               <span className={style.dot1} style={getStyle()}></span>
+               <span className={style.dot2} style={getStyle()}></span>
              </span>
     }
     return <span>
-             <span className={style.bounce1} style={this.getStyle()}></span>
-             <span className={style.bounce2} style={this.getStyle()}></span>
-             <span className={style.bounce3} style={this.getStyle()}></span>
+             <span className={style.bounce1} style={getStyle()}></span>
+             <span className={style.bounce2} style={getStyle()}></span>
+             <span className={style.bounce3} style={getStyle()}></span>
            </span>
   }
 
-  render() {
-    const cx = classNames.bind(style)
-    const loadingClass = this.state.loading ? style['loading'] : null
-    const spinnerPosition = this.props.position ? style[this.props.position] : style['absolute']
-    const spinnerWrap = cx(style['spinner-wrap'], loadingClass, spinnerPosition, this.props.optClass)
-    const spinnerClass = cx(style[this.props.type])
-
-    return (
-      <span className={spinnerWrap}>
-        <span className={spinnerClass}>
-          {this.innerHtml()}
-        </span>
+  return (
+    <span className={spinnerWrap}>
+      <span className={spinnerClass}>
+        {innerHtml()}
       </span>
-    )
-  }
+    </span>
+  )
+}
+
+Spinner.defaultProps = {
+  loading: false
+}
+
+Spinner.propTypes = {
+  /**
+   * Whether the spinner is displayed
+   */
+  loading: React.PropTypes.bool,
+  /**
+   * CSS positioning options for the loader. By default, the spinner will be positioned
+   * in the center of any element with relative positioning.
+   */
+  position: React.PropTypes.oneOf(['fixed', 'inline']),
+  /**
+   * The type of loader you want to display.
+   */
+  type: React.PropTypes.oneOf(['spinner-dots', 'spinner-bounce']).isRequired,
+  /**
+   * The hex code of the loader color.
+   */
+  color: React.PropTypes.string,
+  /**
+   * Optional styles to add to the spinner.
+   */
+  optClass: React.PropTypes.string
 }
 
 export default Spinner
