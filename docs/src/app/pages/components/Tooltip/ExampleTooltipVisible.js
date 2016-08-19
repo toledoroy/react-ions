03e8d4fb9955b207e741aed806ca82251afdd1fb
Sam Ageloff <sam@getambassador.com>
2016-08-19 14:35:05 -0400

    additional tests

--- docs/src/app/pages/components/Tooltip/ExampleTooltipVisible.js
+++ docs/src/app/pages/components/Tooltip/ExampleTooltipVisible.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import Tooltip from 'react-conventions/lib/Tooltip'
+import Button from 'react-conventions/lib/Button'
 
-const ExampleTooltipVisible = () => (
-  <p>Umami four dollar toast craft beer polaroid <Tooltip content="Visible tooltip" tooltipPlacement={'top'} show={true}><a href="#">visible</a></Tooltip> tooltip.</p>
-)
+class ExampleTooltipVisible extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  state = {
+    showing: true
+  }
+
+  handleVisibility = () => {
+    this.state.showing ? this.setState({ showing: false }) : this.setState({ showing: true })
+  }
+
+  render() {
+    return(
+      <div>
+        <p>Umami four dollar toast craft beer polaroid <Tooltip content="Visible tooltip" tooltipPlacement={'top'} show={this.state.showing}><a href="#">visible</a></Tooltip> tooltip.</p>
+        <Button onClick={this.handleVisibility}>{this.state.showing ? 'Hide' : 'Show'} Tooltip</Button>
+      </div>
+    )
+  }
+}
 
 export default ExampleTooltipVisible
--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import throttle from 'lodash/throttle'
 import classNames from 'classnames/bind'
 import style from './style.scss'
 import RenderToLayer from '../internal/RenderToLayer'
@@ -8,7 +9,8 @@ import RenderToLayer from '../internal/RenderToLayer'
  */
 class Tooltip extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.throttle = throttle(this.showTooltip, 500, {trailing: true})
   }
 
   state = {
@@ -40,68 +42,80 @@ class Tooltip extends React.Component {
      * Whether to show the tooltip element by default.
      */
     show: React.PropTypes.bool
-  };
-
-  showTooltip = () => {
-    this.setState({ showing: true });
   }
 
-  hideTooltip = () => {
-    !this.props.show ? this.setState({ showing: false }) : null
-  }
-
-  componentWillUpdate = () => {
-    this.tooltipPlacement()
-  }
-
-  tooltipPlacement = () => {
-    var triggerRect = this._triggerElement.getBoundingClientRect();
-    this._tooltipPlacement = {};
-
-    switch (this.props.tooltipPlacement) {
-      case 'bottom':
-        this._tooltipPlacement.left = triggerRect.left + (triggerRect.right - triggerRect.left) / 2;
-        this._tooltipPlacement.top = triggerRect.bottom;
-        break
-      case 'right':
-        this._tooltipPlacement.left = triggerRect.right;
-        this._tooltipPlacement.top = triggerRect.top + (triggerRect.bottom - triggerRect.top) / 2;
-        break
-      case 'left':
-        this._tooltipPlacement.left = triggerRect.left;
-        this._tooltipPlacement.top = triggerRect.top + (triggerRect.bottom - triggerRect.top) / 2;
-        break
-      default:
-        this._tooltipPlacement.left = triggerRect.left + (triggerRect.right - triggerRect.left) / 2;
-        this._tooltipPlacement.top = triggerRect.top;
+  componentDidMount = () => {
+    if (this.props.show) {
+      window.addEventListener('resize', this.throttle)
     }
-  }
 
-  handleRef = (c) => {
-    this._triggerElement = c
     setTimeout(() => {
       this.tooltipPlacement()
       this.props.show ? this.showTooltip() : null
     }, 1000)
   }
 
+  componentWillReceiveProps = (nextProps) => {
+    this.setState({ showing: nextProps.show })
+  }
+
+  componentWillUnmount = () => {
+    if (this.props.show) {
+      window.removeEventListener('resize', this.throttle)
+    }
+  }
+
+  showTooltip = () => {
+    this.tooltipPlacement()
+    this.setState({ showing: true })
+  }
+
+  hideTooltip = () => {
+    if (!this.props.show) {
+      this.setState({ showing: false })
+    }
+  }
+
+  tooltipPlacement = () => {
+    var triggerRect = this._triggerElement.getBoundingClientRect()
+    this._tooltipPlacement = {}
+
+    switch (this.props.tooltipPlacement) {
+      case 'bottom':
+        this._tooltipPlacement.left = triggerRect.left + (triggerRect.right - triggerRect.left) / 2
+        this._tooltipPlacement.top = triggerRect.bottom
+        break
+      case 'right':
+        this._tooltipPlacement.left = triggerRect.right
+        this._tooltipPlacement.top = triggerRect.top + (triggerRect.bottom - triggerRect.top) / 2
+        break
+      case 'left':
+        this._tooltipPlacement.left = triggerRect.left
+        this._tooltipPlacement.top = triggerRect.top + (triggerRect.bottom - triggerRect.top) / 2
+        break
+      default:
+        this._tooltipPlacement.left = triggerRect.left + (triggerRect.right - triggerRect.left) / 2
+        this._tooltipPlacement.top = triggerRect.top
+    }
+  }
+
   getStyles = () => {
-    var style = {};
+    var style = {}
 
     if (this.state.showing) {
-      style.top = this._tooltipPlacement.top + window.pageYOffset;
-      style.left = this._tooltipPlacement.left + window.pageXOffset;
-      style.opacity = 0.9;
+      style.top = this._tooltipPlacement.top + window.pageYOffset
+      style.left = this._tooltipPlacement.left + window.pageXOffset
+      style.opacity = 0.9
     }
 
-    return style;
+    return style
   }
 
   renderTooltip = () => {
-    const cx = classNames.bind(style);
-    const tooltipShowingClass = this.state.showing ? style['tooltip-showing'] : '';
-    const tooltipClass = cx(style['tooltip-component'], this.props.optClass, tooltipShowingClass, style[this.props.tooltipPlacement]);
-    const styles = this.getStyles();
+    const cx = classNames.bind(style)
+    const tooltipShowingClass = this.state.showing ? style['tooltip-showing'] : ''
+    const tooltipClass = cx(style['tooltip-component'], this.props.optClass, tooltipShowingClass, style[this.props.tooltipPlacement])
+    const styles = this.getStyles()
 
     return (
       <span className={tooltipClass} style={styles}>
@@ -112,7 +126,7 @@ class Tooltip extends React.Component {
 
   render() {
     return (
-      <span onMouseOver={this.showTooltip} onMouseOut={this.hideTooltip} ref={this.handleRef}>
+      <span onMouseOver={this.showTooltip} onMouseOut={this.hideTooltip} ref={(c) => this._triggerElement = c}>
         {this.props.children}
         {this.props.appendToBody ? <RenderToLayer render={this.renderTooltip} open={true} /> : this.renderTooltip()}
       </span>
