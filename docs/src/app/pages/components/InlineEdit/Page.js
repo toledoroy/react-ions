1116e87fba39e6c9eedd7186a22d6312ec5762bd
jakedunahee <jake@getambassador.com>
2016-08-29 09:02:17 -0400

    [#128865231] Completed page for examples

--- docs/src/app/pages/components/InlineEdit/Page.js
+++ docs/src/app/pages/components/InlineEdit/Page.js
@@ -4,17 +4,30 @@ import docs from '!!docgen!react-conventions/lib/Icon/Icon'
 import CodeExample from 'private/modules/CodeExample'
 import ExampleInlineEditDefault from './ExampleInlineEditDefault'
 import exampleInlineEditDefaultCode from '!raw!./ExampleInlineEditDefault'
+import ExampleInlineEditCallback from './ExampleInlineEditCallback'
+import exampleInlineEditCallbackCode from '!raw!./ExampleInlineEditCallback'
 import styles from 'private/css/content'
+
+const description = {
+  inlineEditDefault: 'This is the default **inline edit component**.',
+  inlineEditCallback: 'This is the **inline edit component** with a callback function to edit.',
+};
 
 const InlineEditPage = () => (
   <div>
     <div className={styles.content}>
       <div className={styles.block}>
         <CodeExample
-          title='Default Icon'
-          description='test'
+          title='Default Inline Edit'
+          description={description.inlineEditDefault}
           markup={exampleInlineEditDefaultCode}>
           <ExampleInlineEditDefault />
+        </CodeExample>
+        <CodeExample
+          title='Inline Edit with Callback to Edit'
+          description={description.inlineEditCallback}
+          markup={exampleInlineEditCallbackCode}>
+          <ExampleInlineEditCallback />
         </CodeExample>
       </div>
     </div>
