3a2e41007e45cecd9c6cfb1dcc46f7edd3896f3e
Sam Ageloff <sam@getambassador.com>
2016-09-28 09:19:31 -0400

    removing mount from enzyme import

--- test/Breadcrumb.js
+++ test/Breadcrumb.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { shallow, mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import Breadcrumb from '../src/components/Breadcrumb/Breadcrumb'
 import Icon from '../src/components/Icon/Icon'
 
