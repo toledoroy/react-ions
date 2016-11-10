0cecbc0045323b79f58f0e862b926ac5a347013a
Marko <marko.corokalo@gmail.com>
2016-11-10 18:08:24 +0100

    [#134002673] Don't append Z if the timestamp already has it

--- src/components/internal/TimeString.js
+++ src/components/internal/TimeString.js
@@ -1,22 +1,23 @@
 import moment from 'moment'
 
 const TimeString = (timestamp) => {
-  timestamp = timestamp + 'Z';
+  timestamp = timestamp.slice(-1) === 'Z' ? timestamp : timestamp + 'Z'
+  console.log(timestamp)
 
-  let lessThanOneHour = (moment().diff(timestamp, 'hours') < 1);
-  let taskRelative = moment(timestamp).fromNow();
-  let taskHoursMinutes = moment(timestamp).format('h:mm a');
-  let taskCalendar = moment(timestamp).calendar();
+  let lessThanOneHour = (moment().diff(timestamp, 'hours') < 1)
+  let taskRelative = moment(timestamp).fromNow()
+  let taskHoursMinutes = moment(timestamp).format('h:mm a')
+  let taskCalendar = moment(timestamp).calendar()
 
-  let str;
+  let str
   if (lessThanOneHour) {
-    str = taskRelative;
+    str = taskRelative
   }
   else {
-    str = taskCalendar;
+    str = taskCalendar
   }
 
-  return str;
+  return str
 }
 
 export default TimeString
