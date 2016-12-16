14ef5a6bf98c1886f4f85d686082fcdf76eb44a4
Sam Ageloff <sam@getambassador.com>
2016-12-16 15:21:00 -0500

    adding content to Components page

--- docs/src/app/pages/components/Page.js
+++ docs/src/app/pages/components/Page.js
@@ -5,7 +5,30 @@ const ComponentsPage = () => (
   <div>
     <div className={styles.content}>
       <div className={styles.block}>
-        <h1>Usage</h1>
+        <h3>NPM installation</h3>
+
+        <div className={styles.block}>
+          <p>Install the core package with an NPM client like npm or yarn, pulling in all relevant dependencies:</p>
+          <pre className={styles.editor}>npm install --save react-ions</pre>
+        </div>
+
+        <div className={styles.block}>
+          <p>After installation, you'll be able to import the React components in your application:</p>
+          <pre className={styles.editor}>
+            <div className={styles.line}>import React from 'react'</div>
+            <div className={styles.line}>import Button from 'react-conventions/lib/Button'</div>
+
+            <div className={styles.line}>&nbsp;</div>
+
+            <div className={styles.line}>const ExampleButtonDefault = () =&#62; (</div>
+              <div className={styles.line}>&nbsp;&nbsp;&#60;Button&#62;I am a Button&#60;/Button&#62;</div>
+            <div className={styles.line}>)</div>
+
+            <div className={styles.line}>&nbsp;</div>
+
+            <div className={styles.line}>export default ExampleButtonDefault</div>
+          </pre>
+        </div>
       </div>
     </div>
   </div>
