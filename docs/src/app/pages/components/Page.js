d53718662b32e14450d809febff62baca0560996
Sam Ageloff <sam@getambassador.com>
2016-12-22 12:16:31 -0500

    a few more tweaks

--- docs/src/app/pages/components/Page.js
+++ docs/src/app/pages/components/Page.js
@@ -15,26 +15,25 @@ const ComponentsPage = () => (
       </div>
 
       <div className={styles.block}>
-        <h2>Installation</h2>
+        <h3>Installation</h3>
         <Snippet markup='npm install --save react-ions' />
       </div>
 
       <div className={styles.block}>
-        <h2>Prerequisites</h2>
+        <h3>Prerequisites</h3>
         <p>React&#123;ions&#125; currently requires <b>React 15.1.0</b> (support for 15.2+ coming soon).</p>
-
         <p>We use <a href="https://github.com/css-modules/css-modules">CSS Modules</a> by default to import stylesheets written in SASS. In case you want to import the components already bundled with CSS, your module bundler should be able to require these SASS modules.</p>
       </div>
 
       <div className={styles.block}>
-        <h2>Basic Usage</h2>
+        <h3>Basic Usage</h3>
         <p>In this minimal example, we import a Button with styles already bundled:</p>
         <Snippet markup={buttonExampleSnippet} />
         <Alert type="info" optClass={styles['alert-override']}>If you are not seeing any styles when you load a component, please make sure your project configuration (Webpack, etc.) is using CSS modules to process SASS.</Alert>
       </div>
 
       <div className={styles.block}>
-        <h2>Customizing components</h2>
+        <h3>Customizing components</h3>
         <p>Each React&#123;ion&#125; component exposes an <code className={snippetStyles['snippet-inline']}>optClass</code> prop, to enable style overrides.</p>
         <Snippet markup={inputExampleSnippet} />
         <p>You can now override <code className={snippetStyles['snippet-inline']}>Input</code> styles with the <code className={snippetStyles['snippet-inline']}>.input-overrides</code> class.</p>
