00aea93d54050870792a7b57b1a3493ae82bd595
Thomas Graves <thomaslogangraves@gmail.com>
2017-03-13 11:00:32 -0400

    continuing build on component

--- src/components/Button/ButtonConfirmation.js
+++ src/components/Button/ButtonConfirmation.js
@@ -1,42 +1,22 @@
-import React, { Component } from 'react'
-import Button from 'button'
+import React, { Component, PropTypes } from 'react'
+import ImmutablePropTypes from 'react-immutable-proptypes'
+import Button from '/button'
 import style from './style.scss'
 import classNames from 'classnames/bind'
 
-export ButtonConfirmation extends Component {
-  const cx = classNames.bind(style)
-  const collapseClass = props.collapse ? 'collapse' : null
-  const btnConfirmationClasses = cx(style.btn, props.optClass, props.size, collapseClass)
-
-state = {
-  confirmationOverlayOpen: false,
-  disabled: false
-}
-  handleOpen = (confirm) => {
-    this.setState({ confirmationOverlayOpen: true })
+export class ButtonConfirmation extends Component {
+  constructor(props) {
+    super(props)
   }
 
-
-render = () => {
-  return (
-      this.state.confirmationOverlayOpen
-      ? <div className={style['action-overlay']} style={actionOverlayPosition}>
-          <span>Are you sure?</span>
-          <div className={style['button-wrapper']}>
-            <Button onClick={this.handleConfirmation.bind(this, false)} optClass='danger-alt'>Cancel</Button>
-            <Button onClick={this.handleConfirmation.bind(this, true)}>Yes</Button>
-          </div>
-        </div>
-      : null
-    )
-  }
-}
-
-ButtonConfirmation.propTypes = {
+static propTypes = {
   /**
    * Optional styles to add to the button.
    */
-  optClass: React.PropTypes.string,
+   optClass: React.PropTypes.oneOfType([
+     React.PropTypes.array,
+     React.PropTypes.string
+   ]),
   /**
    * The size of button.
    */
@@ -54,5 +34,45 @@ ButtonConfirmation.propTypes = {
    */
   collapse: React.PropTypes.bool
 }
+
+state = {
+  confirmationOverlayOpen: false,
+  disabled: false
+}
+
+  handleOpen = () => {
+    this.setState({ confirmationOverlayOpen: !this.state.confirmationOverlayOpen })
+  }
+
+  handleConfirmation = (confirm) => {
+    this.props.handleConfirmation(confirm)
+  }
+
+
+render = () => {
+  const cx = classNames.bind(style)
+  const collapseClass = props.collapse ? 'collapse' : null
+  const actionOverlayPosition = { left: this.state.actionOverlayLeft }
+
+  return (
+    <div>
+      <Button onClick={this.handleOpen}>
+        <Icon name={}/>
+        <span>Delete</span>
+      </Button>
+      this.state.confirmationOverlayOpen
+      ? <div className={style['overlay']} style={actionOverlayPosition}>
+          <span>Are you sure?</span>
+          <div className={style['button-wrapper']}>
+            <Button onClick={this.handleConfirmation.bind(this, false)} optClass='danger-alt'>Cancel</Button>
+            <Button onClick={this.handleConfirmation.bind(this, true)}>Yes</Button>
+          </div>
+        </div>
+      : null
+    </div>
+    )
+  }
+}
+
 
 export default ButtonConfirmation
