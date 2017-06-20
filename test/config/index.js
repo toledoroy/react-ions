25df218f9052e35560ae20b0a5c97c7b76cf75d9
Sam Ageloff <sam@getambassador.com>
2017-06-20 14:27:00 -0400

    enabling tests

--- test/config/index.js
+++ test/config/index.js
@@ -1,12 +1,10 @@
-require('../Typeahead.js')
+// require all test .js files
+const testsContext = require.context('../', false, /\.js/)
+//NOTE: in the interest of speed, to test an individual component, use something like this:
+//const testsContext = require.context('../', false, /\RadioGroup.js/);
 
-// // require all test .js files
-// const testsContext = require.context('../', false, /\.js/)
-// //NOTE: in the interest of speed, to test an individual component, use something like this:
-// //const testsContext = require.context('../', false, /\RadioGroup.js/);
-//
-// testsContext.keys().forEach(testsContext)
-//
-// // require all src/components .js files
-// const componentsContext = require.context('../../src/components/', true, /\.js/)
-// componentsContext.keys().forEach(componentsContext)
+testsContext.keys().forEach(testsContext)
+
+// require all src/components .js files
+const componentsContext = require.context('../../src/components/', true, /\.js/)
+componentsContext.keys().forEach(componentsContext)
