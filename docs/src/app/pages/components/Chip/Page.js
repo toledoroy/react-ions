1e84c4bd66606e4c4a74578ae70024d8890e06ac
Corey Fields <corey@getambassador.com>
2017-08-14 18:01:11 -0400

    [#643] add onClick prop with example

--- docs/src/app/pages/components/Chip/Page.js
+++ docs/src/app/pages/components/Chip/Page.js
@@ -8,12 +8,15 @@ import ExampleChipLarger from './ExampleChipLarger'
 import exampleChipLargerCode from '!raw!./ExampleChipLarger'
 import ExampleChipSmaller from './ExampleChipSmaller'
 import exampleChipSmallerCode from '!raw!./ExampleChipSmaller'
+import ExampleChipCallback from './ExampleChipCallback'
+import exampleChipCallbackCode from '!raw!./ExampleChipCallback'
 import styles from 'private/css/content'
 
 const description = {
   chipDefault: 'This is the `chip component` as it appears by default.',
   chipLarger: 'This is the larger `chip component`.',
-  chipSmaller: 'This is the smaller `chip component`.'
+  chipSmaller: 'This is the smaller `chip component`.',
+  chipSmaller: 'This is `chip component` with a click callback.'
 }
 
 const ChipPage = () => (
@@ -37,6 +40,12 @@ const ChipPage = () => (
           description={description.chipSmaller}
           markup={exampleChipSmallerCode}>
           <ExampleChipSmaller />
+        </CodeExample>
+        <CodeExample
+          title='Chip with click callback'
+          description={description.chipClickCallback}
+          markup={exampleChipCallbackCode}>
+          <ExampleChipCallback />
         </CodeExample>
         <div className={styles.block}>
           <h3>Props</h3>
