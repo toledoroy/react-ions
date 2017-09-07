b81b12fba2b3ab994773c3483e272eb1cdffea62
Sam Ageloff <sam@getambassador.com>
2017-09-07 10:57:25 -0400

    adding padding/negative margin

--- docs/src/app/pages/components/Toggle/ExampleToggleDefault.js
+++ docs/src/app/pages/components/Toggle/ExampleToggleDefault.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Toggle from 'react-ions/lib/components/Toggle'
 
 const ExampleToggleDefault = () => (
-  <Toggle label='Would you like to set this?' />
+  <Toggle />
 )
 
 export default ExampleToggleDefault
