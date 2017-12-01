938c39f4b09b5ccc94e35767a797fb0dcfa94aab
Sam Ageloff <sam@getambassador.com>
2017-12-01 14:25:16 -0500

    enabling all tests

--- test/config/index.js
+++ test/config/index.js
@@ -1,12 +1,10 @@
-require('../DatePicker.js')
+//require all test .js files
+const testsContext = require.context('../', false, /\.js/)
+//NOTE: in the interest of speed, to test an individual component, use something like this:
+// const testsContext = require.context('../', false, /\Input.js/);
 
-// //require all test .js files
-// const testsContext = require.context('../', false, /\.js/)
-// //NOTE: in the interest of speed, to test an individual component, use something like this:
-// // const testsContext = require.context('../', false, /\Input.js/);
+testsContext.keys().forEach(testsContext)
 
-// testsContext.keys().forEach(testsContext)
-
-// // require all src/components .js files
-// const componentsContext = require.context('../../src/components/', true, /\.js/)
-// componentsContext.keys().forEach(componentsContext)
+// require all src/components .js files
+const componentsContext = require.context('../../src/components/', true, /\.js/)
+componentsContext.keys().forEach(componentsContext)
