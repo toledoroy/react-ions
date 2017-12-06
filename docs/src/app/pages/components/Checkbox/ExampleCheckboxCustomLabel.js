cb5677138737114daf340eaeff0a91cb7097055d
Corey Fields <corey@getambassador.com>
2017-12-06 18:12:20 -0500

    change link

--- docs/src/app/pages/components/Checkbox/ExampleCheckboxCustomLabel.js
+++ docs/src/app/pages/components/Checkbox/ExampleCheckboxCustomLabel.js
@@ -3,7 +3,7 @@ import Checkbox from 'react-ions/lib/components/Checkbox'
 
 const ExampleCheckboxDefault = () => (
   <Checkbox
-  	label={<span>Label with a <a href='www.test.com' target='_blank' style={{position: 'relative', zIndex: '3'}}>link</a></span>}
+  	label={<span>Label with a <a href='http://www.getambassador.com' target='_blank' style={{position: 'relative', zIndex: '3'}}>link</a></span>}
   	value={false} />
 )
 
