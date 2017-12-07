54820d4eb2077b8eee2d5e853834b30cfc36aa54
Marko <marko.corokalo@gmail.com>
2017-12-07 11:14:48 +0100

    Fix indentation

--- docs/src/app/pages/components/Checkbox/ExampleCheckboxCustomLabel.js
+++ docs/src/app/pages/components/Checkbox/ExampleCheckboxCustomLabel.js
@@ -3,8 +3,9 @@ import Checkbox from 'react-ions/lib/components/Checkbox'
 
 const ExampleCheckboxDefault = () => (
   <Checkbox
-  	label={<span>Label with a <a href='http://www.getambassador.com' target='_blank' style={{position: 'relative', zIndex: '3'}}>link</a></span>}
-  	value={false} />
+    label={<span>Label with a <a href='http://www.getambassador.com' target='_blank' style={{position: 'relative', zIndex: '3'}}>link</a></span>}
+    value={false}
+  />
 )
 
 export default ExampleCheckboxDefault
