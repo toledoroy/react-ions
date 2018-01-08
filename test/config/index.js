370668f6cd16dd2f45ba0c05354da0b6121be7a8
Sam Ageloff <sam@getambassador.com>
2018-01-08 11:51:11 -0500

    fixing radio and radio group tests

--- test/config/index.js
+++ test/config/index.js
@@ -1,17 +1,19 @@
+require('../RadioGroup.js')
 
-// TODO: reorganize test files so that we don't need multiple context entries here
-const componentTestContext = require.context('../', false, /\.js/)
-const utilityTestContext = require.context('../utilities', false, /\.js/)
 
-// NOTE: in the interest of speed, to test an individual component, use something like this:
-// const testsContext = require.context('../', false, /\Input.js/);
+// // TODO: reorganize test files so that we don't need multiple context entries here
+// const componentTestContext = require.context('../', false, /\.js/)
+// const utilityTestContext = require.context('../utilities', false, /\.js/)
 
-componentTestContext.keys().forEach(componentTestContext)
-utilityTestContext.keys().forEach(utilityTestContext)
+// // NOTE: in the interest of speed, to test an individual component, use something like this:
+// // const testsContext = require.context('../', false, /\Input.js/);
 
-// require all source files for code coverage
-const componentsContext = require.context('../../src/components/', true, /\.js/)
-const utilitiesContext = require.context('../../src/utilities/', true, /\.js/)
+// componentTestContext.keys().forEach(componentTestContext)
+// utilityTestContext.keys().forEach(utilityTestContext)
 
-utilitiesContext.keys().forEach(utilitiesContext)
-componentsContext.keys().forEach(componentsContext)
+// // require all source files for code coverage
+// const componentsContext = require.context('../../src/components/', true, /\.js/)
+// const utilitiesContext = require.context('../../src/utilities/', true, /\.js/)
+
+// utilitiesContext.keys().forEach(utilitiesContext)
+// componentsContext.keys().forEach(componentsContext)
