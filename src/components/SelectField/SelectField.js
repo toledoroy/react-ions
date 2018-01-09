28373db7f91b8fcc500ca683798f360141ceb430
William Hitchcock <willh@getambassador.com>
2018-01-09 17:59:38 -0500

    Fix className Fix missing key

--- src/components/SelectField/SelectField.js
+++ src/components/SelectField/SelectField.js
@@ -172,7 +172,21 @@ export class SelectField extends React.Component {
     const { valueProp, hideProp, label } = this.props
 
     let options = this.props.options.map((option, index) =>
-      <li key={index} onClick={this.selectOption.bind(null, option, true)} className={hideProp && option[hideProp] && style['hidden']}>{option.icon ? <Icon name={option.icon} fill={option.iconColor ||  null} className={style.icon} height='16' width='16' /> : null}{option[this.props.displayProp]}</li>
+      <li
+        key={index}
+        onClick={this.selectOption.bind(null, option, true)}
+        className={(hideProp && option[hideProp]) ? style['hidden'] : undefined}>
+          {option.icon
+            ? <Icon
+              name={option.icon}
+              fill={option.iconColor ||  null}
+              className={style.icon}
+              height='16'
+              width='16' />
+            : null
+          }
+          {option[this.props.displayProp]}
+        </li>
     )
 
     if (options.length === 0) {
