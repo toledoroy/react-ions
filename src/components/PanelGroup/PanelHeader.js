2188d4b6dcce964d6e149dc816debde8afe72835
William Hitchcock <willh@getambassador.com>
2018-01-17 17:50:17 -0500

    fix test, formatting

--- src/components/PanelGroup/PanelHeader.js
+++ src/components/PanelGroup/PanelHeader.js
@@ -55,7 +55,14 @@ class PanelHeader extends React.Component {
           ? <div>
             <div className={style['title-group']}>
               {this.props.contextNode ? <div className={style['context-node']}>{this.props.contextNode}</div> : null}
-              {this.props.contextIcon ? <div className={style['context-icon']}><Icon name={this.props.contextIcon} height={this.props.contextIconSize} width={this.props.contextIconSize} /></div> : null}
+              {this.props.contextIcon
+                ? <div className={style['context-icon']}>
+                    <Icon
+                      name={this.props.contextIcon}
+                      height={this.props.contextIconSize}
+                      width={this.props.contextIconSize} />
+                  </div>
+                : null}
               {this.props.title ? <h4>{this.props.title}</h4> : null}
             </div>
             <div className={style['toggle-icon']}>
