3bd9feaefd3116fc9c621c8097cca21f8e2e75bb
Marko <marko.corokalo@gmail.com>
2018-01-24 16:28:57 +0100

    [#1260] Allow breadcrumb items to be clickable

--- docs/src/app/layout/Base.js
+++ docs/src/app/layout/Base.js
@@ -56,7 +56,7 @@ class Base extends React.Component {
           <div className={!this.state.sidebarCollapsed ? contentClass : contentClassSidebarActive}>
             { currentBasePage
             ? <div className={!this.state.breadcrumbActive ? breadcrumbClass : breadcrumbActive}>
-              <Breadcrumb routes={this.props.routes} gradientColor='#ECF0F0' padding={15} />
+              <Breadcrumb routes={this.props.routes} gradientColor='#ECF0F0' padding={15} clickable={true} />
             </div>
             : null
             }
