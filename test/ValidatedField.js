c7e1571416b00f31279758ee2cd250d1c34b9193
Marko <marko.corokalo@gmail.com>
2018-02-07 15:03:25 +0100

    [#1321] Update test

--- test/ValidatedField.js
+++ test/ValidatedField.js
@@ -10,6 +10,7 @@ describe('ValidatedField', () => {
   it('should shallow render itself', () => {
     wrapper = shallow(<ValidatedInput error='Error here.' />)
 
+    expect(wrapper.hasClass('validated-field')).to.be.true
     expect(wrapper.find(Input)).to.be.length(1)
     expect(wrapper.childAt(0).hasClass('has-error')).to.be.true
     expect(wrapper.childAt(1).hasClass('has-error__message')).to.be.true
