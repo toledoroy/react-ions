77f9e3287c0541c11702a378414f1779ed30e85e
Sam Ageloff <sam@getambassador.com>
2018-05-09 10:23:24 -0400

    updating social icon

--- docs/src/app/pages/components/Icons/ExampleIconCustom.js
+++ docs/src/app/pages/components/Icons/ExampleIconCustom.js
@@ -2,7 +2,19 @@ import React from 'react'
 import Icon from 'react-ions/lib/components/Icon'
 
 const ExampleIconCustom = () => (
-  <Icon name='md-check' width='48' height='48' fill='#FFE779' className='custom' />
+  <div>
+    <Icon name='mbsy-facebook' width='48' height='48' fill='#FFE779' />
+    <Icon name='mbsy-twitter' width='48' height='48' fill='#FFE779' />
+    <Icon name='mbsy-dwolla' width='48' height='48' fill='#FFE779' />
+    <Icon name='mbsy-caret' width='48' height='48' fill='#FFE779' />
+    <Icon name='mbsy-webhooks' width='48' height='48' fill='#FFE779' />
+    <Icon name='mbsy-google' width='48' height='48' fill='#FFE779' />
+    <Icon name='mbsy-paypal' width='48' height='48' fill='#FFE779' />
+    <Icon name='mbsy-slack' width='48' height='48' fill='#FFE779' />
+    <Icon name='mbsy-yahoo' width='48' height='48' fill='#FFE779' />
+    <Icon name='mbsy-klout' width='48' height='48' fill='#FFE779' />
+    <Icon name='mbsy-linkedin' width='48' height='48' fill='#FFE779' />
+  </div>
 )
 
 export default ExampleIconCustom
