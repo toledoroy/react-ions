198d43b82d97c0309820d2ae7a43c574b674de8f
Sam Ageloff <sam@getambassador.com>
2018-05-09 11:32:02 -0400

    reverting doc example

--- docs/src/app/pages/components/Icons/ExampleIconCustom.js
+++ docs/src/app/pages/components/Icons/ExampleIconCustom.js
@@ -2,19 +2,7 @@ import React from 'react'
 import Icon from 'react-ions/lib/components/Icon'
 
 const ExampleIconCustom = () => (
-  <div>
-    <Icon name='mbsy-facebook' width='48' height='48' fill='#FFE779' />
-    <Icon name='mbsy-twitter' width='48' height='48' fill='#FFE779' />
-    <Icon name='mbsy-dwolla' width='48' height='48' fill='#FFE779' />
-    <Icon name='mbsy-caret' width='48' height='48' fill='#FFE779' />
-    <Icon name='mbsy-webhooks' width='48' height='48' fill='#FFE779' />
-    <Icon name='mbsy-google' width='48' height='48' fill='#FFE779' />
-    <Icon name='mbsy-paypal' width='48' height='48' fill='#FFE779' />
-    <Icon name='mbsy-slack' width='48' height='48' fill='#FFE779' />
-    <Icon name='mbsy-yahoo' width='48' height='48' fill='#FFE779' />
-    <Icon name='mbsy-klout' width='48' height='48' fill='#FFE779' />
-    <Icon name='mbsy-linkedin' width='48' height='48' fill='#FFE779' />
-  </div>
+  <Icon name='md-check' width='48' height='48' fill='#FFE779' className='custom' />
 )
 
 export default ExampleIconCustom
