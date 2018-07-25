236889beab39979d5e1999add5a5d0fb81b9cd73
Corey Fields <corey@getambassador.com>
2018-07-25 10:57:05 -0400

    no message

--- docs/src/app/pages/components/InlineEdit/ExampleInlineEditError.js
+++ docs/src/app/pages/components/InlineEdit/ExampleInlineEditError.js
@@ -10,28 +10,33 @@ class ExampleInlineEditError extends React.Component {
   state = {
     inlineValue: 'I will have an error after saving',
     loading: false,
-    error: ''
+    error: '',
+    count: 0
   }
 
   handleSave = event => {
     if (event.target.name === 'test') {
-      this.setState({ loading: true, error: '' })
+      console.log('setting state', this.state.count++)
+      this.setState({ loading: true, error: '', count: this.state.count++ }, () => {
 
-      if (event.target.canceled) {
-        console.log('reset error')
-        this.setState({ loading: false, error: '' })
-      } else {
-        setTimeout(() => {
-          this.setState({ loading: false, error: 'This is an error' })
-        }, 3000)
-      }
+        if (event.target.canceled) {
+          console.log('reset error')
+          this.setState({ loading: false, error: '' })
+        } else {
+          setTimeout(() => {
+            console.log(this.state.count)
+            this.setState({ loading: false, error: this.state.count > 1 ? '' : 'This is an error' })
+          }, 1000)
+        }
+
+      })
     }
   }
 
   render() {
     return (
       <div>
-        <InlineEdit name='test' value={this.state.inlineValue} changeCallback={this.handleSave} placeholder='Custom Placeholder' loading={this.state.loading} error={this.state.error} />
+        <InlineEdit copyToClipboard name='test' value={this.state.inlineValue} changeCallback={this.handleSave} placeholder='Custom Placeholder' loading={this.state.loading} error={this.state.error} />
       </div>
     )
   }
