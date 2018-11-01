3ac0608bb9bc79325e1bfa9282029a7092c73643
Sam Ageloff <samuelageloff@gmail.com>
2018-11-01 13:00:10 -0400

    updating regex for webpack karma coverage, to allow for jest snapshots

--- test/config/index.js
+++ test/config/index.js
@@ -10,8 +10,11 @@ componentTestContext.keys().forEach(componentTestContext)
 utilityTestContext.keys().forEach(utilityTestContext)
 
 // require all source files for code coverage
-const componentsContext = require.context('../../src/components/', true, /\.js/)
-const utilitiesContext = require.context('../../src/utilities/', true, /\.js/)
+// ^(?!__tests__).+\.js matches any .js files that are not in a __tests__ directory
+// allows us to store our Jest tests in the same dircetory, 
+// while also generating Karma test coverage.
+const componentsContext = require.context('../../src/components/', true, /\^(?!__tests__).+\.js/)
+const utilitiesContext = require.context('../../src/utilities/', true, /\^(?!__tests__).+\.js/)
 
 utilitiesContext.keys().forEach(utilitiesContext)
 componentsContext.keys().forEach(componentsContext)
