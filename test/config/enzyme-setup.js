e7db63df9a979896f81cd1404991634ae5ec61bd
Sam Ageloff <samuelageloff@gmail.com>
2018-11-01 10:20:30 -0400

    adjusting path to framework setup file

--- test/config/enzyme-setup.js
+++
@@ -1,8 +0,0 @@
-// Using this file instead of the karma conf, to handle
-// shallow rendering components in Jest tests
-
-import Enzyme from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
-
-// Setup enzyme's react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() })
