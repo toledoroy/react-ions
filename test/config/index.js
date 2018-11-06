31d5d521e370e1bc6a33fdd95e5b50fd174eaf81
Sam Ageloff <samuelageloff@gmail.com>
2018-11-06 10:11:01 -0500

    adjusting coverage context regex

--- test/config/index.js
+++ test/config/index.js
@@ -13,8 +13,8 @@ utilityTestContext.keys().forEach(utilityTestContext)
 // ^(?!__tests__).+\.js matches any .js files that are not in a __tests__ directory
 // allows us to store our Jest tests in the same dircetory, 
 // while also generating Karma test coverage.
-const componentsContext = require.context('../../src/components/', true, /^(?!\.\/index\.js)((?!__tests__).)*\.js$/)
-const utilitiesContext = require.context('../../src/utilities/', true, /^(?!\.\/index\.js)((?!__tests__).)*\.js$/)
+const componentsContext = require.context('../../src/components/', true, /^((?!__tests__).)*\.js$/)
+const utilitiesContext = require.context('../../src/utilities/', true, /^((?!__tests__).)*\.js$/)
 
 utilitiesContext.keys().forEach(utilitiesContext)
 componentsContext.keys().forEach(componentsContext)
