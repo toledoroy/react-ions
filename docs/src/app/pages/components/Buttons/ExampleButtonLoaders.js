6f9a270606e9ce92c5f81360730ea543985e77dc
Sam Ageloff <samuelageloff@gmail.com>
2019-02-14 12:32:00 -0500

    adjusting something

--- docs/src/app/pages/components/Buttons/ExampleButtonLoaders.js
+++ docs/src/app/pages/components/Buttons/ExampleButtonLoaders.js
@@ -3,7 +3,7 @@ import React from 'react'
 class ExampleButtonLoaders extends React.Component {
   render() {
     return (
-      <iframe src='http://localhost:3000/components/test' width='100%' height='500px'/>
+      <iframe src='components/test' width='100%' height='500px'/>
     )
   }
 }
